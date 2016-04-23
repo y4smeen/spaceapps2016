5acff81d6d359944e1a2b8dde715f52ef6a00500
Yasmeen Roumie <y4smeen@gmail.com>
2016-04-23 15:38:02 -0400

    media msg

--- run.py
+++ run.py
@@ -8,7 +8,15 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    with resp.message("Hello, Mobile Monkey") as m:
+        m.media("https://demo.twilio.com/owl.png")
+    return str(resp)
+
+@app.route("/nearme", methods=['GET', 'POST'])
+def nearme():
+    resp = twilio.twiml.Response()
+    natural_disaster = []
+    resp.message(natural_disaster[0] + " " + natural_disaster[1] + " miles away")
     return str(resp)
 
 if __name__ == "__main__":
